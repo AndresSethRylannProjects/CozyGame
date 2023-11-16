7a09ce93f97216636e60a88cc49fde14e221e3ca
agonzal1 <agonzal1@ualberta.ca>
2023-11-15 18:32:24 -0700

    Sprite Splitting + Movement
    Sprite Splitting + Value Initialization for Movement

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -6,6 +6,12 @@ public class movement : MonoBehaviour
 {
     public Rigidbody2D body;
     public SpriteRenderer spriteRenderer;
+    // To be changed i.e only temporary movement script
+    public List<Sprite> northSprites;
+    public List<Sprite> northEastSprites;
+    public List<Sprite> southSprites;
+    public List<Sprite> southEastSprites;
+    public List<Sprite> eastSprites;
     
     // Start is called before the first frame update
     void Start()
