02d88dd1ecd313b954c23c65b5a3fb62777b8663
agonzal1 <agonzal1@ualberta.ca>
2024-03-26 11:03:38 -0600

    Added basic Timer Functionality
    Menu button now enables timer, see script comments for expected future behaviour.
    
    Should also make it look nicer

---
+++ Assets/Scripts/PomodoroTimer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PomodoroTimer : MonoBehaviour
+{
+    public bool hasFinished = true; // change to false in the future
+    [SerializeField] float remainingTime;
+    public TextMesh timerText;
+    public bool tick = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // not sure if I want something here yet
+    }
+
+    // start timer
+    public void startTimer(){
+        
+        if(!hasFinished){
+            // ask user if they are sure they want to reset the timer
+            // if yes, then reset the timer
+            // if no, then do nothing
+        }
+        else {
+            // start timer
+            remainingTime = 3600;
+            tick = true;
+            // make timer text visible
+            timerText.gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(tick){
+            if (remainingTime > 0){
+                remainingTime -= Time.deltaTime;
+            }
+            else if (remainingTime < 0){
+                remainingTime = 0;
+            }
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
